e1aba650eb6d2c6483c617ac4b416191bc7f6abc
Roy1419 <royshubham471@gmail.com>
2025-01-06 21:35:18 +0530

    this commit is for  data base schema degin folder

---
+++ models/user.model.ts
@@ -0,0 +1,30 @@
+import mongoose, { models, Schema } from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser {
+    email: string;
+    password: string;
+    role: "user" | "admin";
+    _id?: mongoose.Types.ObjectId;
+    createdAt?: Date;
+    updatedAt?: Date;
+};
+
+const userSchema = new Schema<IUser>({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, unique: true},
+    role: {type: String, enum:["user", "admin"], default: "user" },
+}, {
+    timestamps: true
+});
+
+userSchema.pre("save", async function(next) {
+    if(this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+const User = models?.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
