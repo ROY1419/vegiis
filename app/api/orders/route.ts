d5ffa476c0c800d3444470cddc8a5f0f9bb277f8
Roy1419 <royshubham471@gmail.com>
2025-01-16 20:56:07 +0530

    this commit is for modification for order route file

--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,39 +1,39 @@
 import { authOptions } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
-import Order from "@/models/order.model";
+import { Order } from "@/models/order.model";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import Razorpay from "razorpay";
 
 const razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
+    key_id: process.env.RAZORPAY_KEY_ID!,
     key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 export async function POST(req: NextRequest) {
     try {
         const session = await getServerSession(authOptions)
-        if(!session){
-            return new NextResponse("Unauthorized", {status: 401})
+        if (!session) {
+            return new NextResponse("Unauthorized", { status: 401 })
         }
-        const {productId, variant} =  await req.json()
+        const { productId, variant } = await req.json()
         await connectToDatabase()
         // Create Razorpay order
-        const order =  await razorpay.orders.create({
+        const order = await razorpay.orders.create({
             amount: Math.round(variant.price * 50),
             currency: "INR",
-            receipt: "order_rcptid_11",
-            notes:{
-                productID : productId.toString(),
+            receipt:`recept-${Date.now()}`,
+            notes: {
+                productID: productId.toString(),
             },
         })
         const newOrder = await Order.create({
             userId: session.user.id,
             productId,
             variant,
-            amount: variant.price,
+            amount: Math.round(variant.price * 50),
             razorpayOrderId: order.id,
-            status : "pending",
+            status: "pending",
         })
         return NextResponse.json({
             order: order.id,
@@ -45,6 +45,6 @@ export async function POST(req: NextRequest) {
     } catch (error) {
         console.error("Error creating order:", error);
         return NextResponse.json({ error: "Faild creating order" }, { status: 500 });
-        
+
     }
 }
