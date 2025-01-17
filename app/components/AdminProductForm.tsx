a3eb05356b95d2cabe7e6e35b5acee2ddac9600c
Roy1419 <royshubham471@gmail.com>
2025-01-17 21:41:46 +0530

    this commit is for implementation of componets folder in app

---
+++ app/components/AdminProductForm.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from 'react'
+import { useNotification } from '../Notification'
+
+export default function AdminProductForm() {
+    const [loading, setLoading] = useState(false)
+    const {showNotification} = useNotification()
+
+
+    return (
+        <div>AdminProductForm</div>
+    )
+}
+
