838bee976c32552ab69b63a4dd98c8f351dae109
Roy1419 <royshubham471@gmail.com>
2025-01-22 22:26:30 +0530

    this commit is for modififcation of product routes

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,29 +1,29 @@
 import { authOptions } from "@/lib/auth";
 import { connectToDatabase } from "@/lib/db";
-import { Product, IProduct } from "@/models/product.model";
-import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import Product, { IProduct } from "@/models/product.model";
+import { getServerSession } from 'next-auth/next'
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
     try {
         await connectToDatabase();
         const products = await Product.find({}).lean()
         if (!products || products.length === 0) {
-            return NextResponse.json({ error: "No product found" }, { status: 404 })
+            return NextResponse.json([], { status: 200 });
         }
-        return NextResponse.json({ products }, { status: 200 })
+        return NextResponse.json(products)
     } catch (error) {
+        console.error("Error fetching products:", error);
         return NextResponse.json(
-            { error: "something went wrong" },
-            { status: 404 }
-        )
-
+            { error: "Failed to fetch products" },
+            { status: 500 }
+        );
     }
 }
 
-export async function POST(request: NextResponse) {
+export async function POST(request: NextRequest) {
     try {
-        const session = await getServerSession(authOptions)
+        const session = await getServerSession(authOptions);
         if (!session || session.user.role !== "admin") {
             return NextResponse.json({ error: "Unauthorized" }, { status: 404 })
         }
@@ -45,14 +45,13 @@ export async function POST(request: NextResponse) {
             }
         }
         const newProduct = await Product.create(body);
-        return NextResponse.json({ newProduct }, { status: 201 })
+        return NextResponse.json( newProduct )
 
     } catch (error) {
         console.error("Error creating product:", error);
         return NextResponse.json(
             { error: "Failed to create product" },
-            { status: 404 }
+            { status: 500 }
         )
     }
-
 }
