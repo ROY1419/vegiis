c862f74164ff838e5dc92910385c4dbc29691640
Roy1419 <royshubham471@gmail.com>
2025-01-24 22:49:24 +0530

    this commit is for modification of app page.tsx file

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,30 @@
-"use client";
-import { apiClient } from "@/lib/api-client";
-import { IProduct } from "@/models/product.model";
-import React,{ useEffect, useState } from "react";
-import ProductGallery from "./components/ProductGallery";
 
+"use client";
+import React, { useEffect, useState } from "react";
+import { apiClient } from "@/lib/api-client";
+import ProductGallery from "./components/ProductGallery";
+import { IProduct } from "@/models/product.model";
 
 export default function Home() {
-  const [products, setProducts] = useState<IProduct[]>([])
-  useEffect(() => {
-    const fetchProduct = async () => {
-      try {
-        const data = await apiClient.getProducts()
-        setProducts(data)
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    }
-    fetchProduct()
-  }, [])
+    const [products, setProducts] = useState<IProduct[]>([]);
 
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const data = await apiClient.getProducts();
+                setProducts(data);
+            } catch (error) {
+                console.error("Error fetching products:", error);
+            }
+        };
 
-  return (
-    <main className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-8">ImageKit Shop</h1>
-      <ProductGallery product={products} />
-    </main>
-  );
+        fetchProducts();
+    }, []);
+
+    return (
+        <main className="container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold mb-8">ImageKit Shop</h1>
+            <ProductGallery product={products} />
+        </main>
+    );
 }
