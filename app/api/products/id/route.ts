06c472b8c4d95ae31b1499b69b46e0c3011cd5b5
Roy1419 <royshubham471@gmail.com>
2025-02-14 22:02:18 +0530

    this commit is for implementation of products-id folder under the api folder of the app directory

---
+++ app/api/products/id/route.ts
@@ -0,0 +1,21 @@
+import { connectToDatabase } from "@/lib/db";
+import Product from "@/models/product.model";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest, props: { params: Promise<{ id: string }> }) {
+    try {
+        const { id } = await props.params;
+        await connectToDatabase();
+        const product = await Product.findById(id).lean();
+        if (!product) {
+            return NextResponse.json({ error: "No product found" }, { status: 404 })
+        }
+        return NextResponse.json(product);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: "Failed to fetch the product" },
+            { status: 500 }
+        )
+    }
+}
